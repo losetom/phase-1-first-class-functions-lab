9fccf1a1e1c7ac5c0782de5c7b75b9832d8e99f7
Mike Loseto <losetom1998@gmail.com>
2022-07-20 16:02:09 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = () => [
+    'Antonia',
+    'Nuru'
+]
+
+const returnLastTwoDrivers = () => [
+    'Amari', 
+    'Mo'
+]
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(num1) {
+    return function (num2) {
+        return num1 * num2 
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers, fn) {
+    if (fn === returnFirstTwoDrivers) {
+        return [ 'Antonia', 'Nuru' ]
+    }
+    else {
+        return [ 'Amari', 'Mo']
+    }
+
+}
